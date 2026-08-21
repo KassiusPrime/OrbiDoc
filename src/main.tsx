5ef36c2d10e940fb83281fdab5318229718307e7
KassiusPrime <cassianokaique9@gmail.com>
2026-08-20 23:16:11 -0300

    Activate the organized OrbiDoc app shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
-import App from './AppV4';
+import App from './AppV5';
 import { AppErrorBoundary } from './components/AppErrorBoundary';
 import { AiRuntimeStatus } from './components/AiRuntimeStatus';
 import { OrbiDocExperienceShell } from './components/OrbiDocExperienceShell';
