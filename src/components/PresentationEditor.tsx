cf7ce3494786320de8eb0c6500bb0649fc22ac79
KassiusPrime <cassianokaique9@gmail.com>
2026-08-25 01:38:02 -0300

    feat: add pro controls to presentation studio

--- src/components/PresentationEditor.tsx
+++ src/components/PresentationEditor.tsx
@@ -1,1 +1,5 @@
-export { PresentationEditorStudio as PresentationEditor } from './PresentationEditorStudio';
+import React from 'react';
+import { PresentationEditorStudio } from './PresentationEditorStudio';
+import { StudioPowerBar } from './StudioPowerBar';
+
+export const PresentationEditor: React.FC<React.ComponentProps<typeof PresentationEditorStudio>> = (props) => <div><StudioPowerBar project={props.project} kind="powerpoint" showNotification={props.showNotification} /><PresentationEditorStudio {...props} /></div>;
