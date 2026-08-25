9f8395851b84876b3b92f1888a955157615f2dd5
KassiusPrime <cassianokaique9@gmail.com>
2026-08-25 05:53:33 -0300

    feat: mount spreadsheet pro tools with safe editor remount

--- src/components/SpreadsheetEditor.tsx
+++ src/components/SpreadsheetEditor.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SpreadsheetEditorStudio } from './SpreadsheetEditorStudio';
 import { StudioPowerBar } from './StudioPowerBar';
+import { SpreadsheetProPanel } from './SpreadsheetProPanel';
 
-export const SpreadsheetEditor: React.FC<React.ComponentProps<typeof SpreadsheetEditorStudio>> = (props) => <div><StudioPowerBar project={props.project} kind="excel" showNotification={props.showNotification} /><SpreadsheetEditorStudio {...props} /></div>;
+export const SpreadsheetEditor: React.FC<React.ComponentProps<typeof SpreadsheetEditorStudio>> = (props) => {
+  const [revision, setRevision] = useState(0);
+  return <div>
+    <StudioPowerBar project={props.project} kind="excel" showNotification={props.showNotification} />
+    <SpreadsheetProPanel project={props.project} onProjectChange={props.onProjectChange} onApplied={() => setRevision((value) => value + 1)} showNotification={props.showNotification} />
+    <SpreadsheetEditorStudio key={`${props.project.id}:${revision}`} {...props} />
+  </div>;
+};
