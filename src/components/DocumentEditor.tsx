ac433a5a7e8e27401c0762198c67daa07563b1ca
KassiusPrime <cassianokaique9@gmail.com>
2026-08-25 05:42:10 -0300

    feat: mount find and replace in document studio

--- src/components/DocumentEditor.tsx
+++ src/components/DocumentEditor.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DocumentEditorStudio } from './DocumentEditorStudio';
+import { DocumentFindReplaceBar } from './DocumentFindReplaceBar';
 import { StudioPowerBar } from './StudioPowerBar';
 
-export const DocumentEditor: React.FC<React.ComponentProps<typeof DocumentEditorStudio>> = (props) => <div><StudioPowerBar project={props.project} kind="word" showNotification={props.showNotification} /><DocumentEditorStudio {...props} /></div>;
+export const DocumentEditor: React.FC<React.ComponentProps<typeof DocumentEditorStudio>> = (props) => <div><StudioPowerBar project={props.project} kind="word" showNotification={props.showNotification} /><DocumentFindReplaceBar showNotification={props.showNotification} /><DocumentEditorStudio {...props} /></div>;
