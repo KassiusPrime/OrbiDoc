3e757c46bbc88eb5a56101a0f299361ea5d0d51a
KassiusPrime <cassianokaique9@gmail.com>
2026-08-25 01:37:56 -0300

    feat: add pro controls to spreadsheet studio

--- src/components/SpreadsheetEditor.tsx
+++ src/components/SpreadsheetEditor.tsx
@@ -1,1 +1,5 @@
-export { SpreadsheetEditorStudio as SpreadsheetEditor } from './SpreadsheetEditorStudio';
+import React from 'react';
+import { SpreadsheetEditorStudio } from './SpreadsheetEditorStudio';
+import { StudioPowerBar } from './StudioPowerBar';
+
+export const SpreadsheetEditor: React.FC<React.ComponentProps<typeof SpreadsheetEditorStudio>> = (props) => <div><StudioPowerBar project={props.project} kind="excel" showNotification={props.showNotification} /><SpreadsheetEditorStudio {...props} /></div>;
