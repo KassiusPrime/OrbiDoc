f05e580b23b714e8f268f5600dcd4a5be35ba504
KassiusPrime <cassianokaique9@gmail.com>
2026-08-25 05:54:36 -0300

    feat: mount presentation pro master controls

--- src/components/PresentationEditor.tsx
+++ src/components/PresentationEditor.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PresentationEditorStudio } from './PresentationEditorStudio';
 import { StudioPowerBar } from './StudioPowerBar';
+import { PresentationProPanel } from './PresentationProPanel';
 
-export const PresentationEditor: React.FC<React.ComponentProps<typeof PresentationEditorStudio>> = (props) => <div><StudioPowerBar project={props.project} kind="powerpoint" showNotification={props.showNotification} /><PresentationEditorStudio {...props} /></div>;
+export const PresentationEditor: React.FC<React.ComponentProps<typeof PresentationEditorStudio>> = (props) => {
+  const [revision, setRevision] = useState(0);
+  return <div>
+    <StudioPowerBar project={props.project} kind="powerpoint" showNotification={props.showNotification} />
+    <PresentationProPanel project={props.project} onProjectChange={props.onProjectChange} onApplied={() => setRevision((value) => value + 1)} showNotification={props.showNotification} />
+    <PresentationEditorStudio key={`${props.project.id}:${revision}`} {...props} />
+  </div>;
+};
