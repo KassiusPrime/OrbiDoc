e1d5dd6b4c730a18b8336c284ca3c7bd9ea7104d
KassiusPrime <cassianokaique9@gmail.com>
2026-08-25 01:37:51 -0300

    feat: add pro controls to document studio

--- src/components/DocumentEditor.tsx
+++ src/components/DocumentEditor.tsx
@@ -1,1 +1,5 @@
-export { DocumentEditorStudio as DocumentEditor } from './DocumentEditorStudio';
+import React from 'react';
+import { DocumentEditorStudio } from './DocumentEditorStudio';
+import { StudioPowerBar } from './StudioPowerBar';
+
+export const DocumentEditor: React.FC<React.ComponentProps<typeof DocumentEditorStudio>> = (props) => <div><StudioPowerBar project={props.project} kind="word" showNotification={props.showNotification} /><DocumentEditorStudio {...props} /></div>;
