9d2fcb1fbea7da135fc6d2b9a594ae91cc97df9a
KassiusPrime <cassianokaique9@gmail.com>
2026-08-26 07:17:22 -0300

    style: scope presentation creator to elite workspace chrome

--- src/components/PresentationEditor.tsx
+++ src/components/PresentationEditor.tsx
@@ -5,7 +5,7 @@ import { PresentationProPanel } from './PresentationProPanel';
 
 export const PresentationEditor: React.FC<React.ComponentProps<typeof PresentationEditorStudio>> = (props) => {
   const [revision, setRevision] = useState(0);
-  return <div>
+  return <div className="orbidoc-creator-shell" data-orbidoc-creator="powerpoint">
     <StudioPowerBar project={props.project} kind="powerpoint" showNotification={props.showNotification} />
     <PresentationProPanel project={props.project} onProjectChange={props.onProjectChange} onApplied={() => setRevision((value) => value + 1)} showNotification={props.showNotification} />
     <PresentationEditorStudio key={`${props.project.id}:${revision}`} {...props} />
