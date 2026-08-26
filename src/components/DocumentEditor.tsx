8ef44ed41ad9f95da6a11f0058f5ae95384f51fb
KassiusPrime <cassianokaique9@gmail.com>
2026-08-26 07:17:00 -0300

    style: scope document creator to elite workspace chrome

--- src/components/DocumentEditor.tsx
+++ src/components/DocumentEditor.tsx
@@ -4,4 +4,11 @@ import { DocumentFindReplaceBar } from './DocumentFindReplaceBar';
 import { DocumentProPanel } from './DocumentProPanel';
 import { StudioPowerBar } from './StudioPowerBar';
 
-export const DocumentEditor: React.FC<React.ComponentProps<typeof DocumentEditorStudio>> = (props) => <div><StudioPowerBar project={props.project} kind="word" showNotification={props.showNotification} /><DocumentFindReplaceBar showNotification={props.showNotification} /><DocumentProPanel showNotification={props.showNotification} /><DocumentEditorStudio {...props} /></div>;
+export const DocumentEditor: React.FC<React.ComponentProps<typeof DocumentEditorStudio>> = (props) => (
+  <div className="orbidoc-creator-shell" data-orbidoc-creator="word">
+    <StudioPowerBar project={props.project} kind="word" showNotification={props.showNotification} />
+    <DocumentFindReplaceBar showNotification={props.showNotification} />
+    <DocumentProPanel showNotification={props.showNotification} />
+    <DocumentEditorStudio {...props} />
+  </div>
+);
