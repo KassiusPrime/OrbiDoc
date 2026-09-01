fc02d50dcccf1f2dc81e30930f818f2405ae5cbb
KassiusPrime <cassianokaique9@gmail.com>
2026-08-31 22:13:35 -0300

    feat(word): route document workspace to Lexical engine

--- src/components/DocumentEditor.tsx
+++ src/components/DocumentEditor.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { DocumentEditorStudio } from './DocumentEditorStudio';
-import { DocumentFindReplaceBar } from './DocumentFindReplaceBar';
-import { DocumentProPanel } from './DocumentProPanel';
+import { DocumentEditorLexical } from './DocumentEditorLexical';
 import { StudioPowerBar } from './StudioPowerBar';
 
-export const DocumentEditor: React.FC<React.ComponentProps<typeof DocumentEditorStudio>> = (props) => <div><StudioPowerBar project={props.project} kind="word" showNotification={props.showNotification} /><DocumentFindReplaceBar showNotification={props.showNotification} /><DocumentProPanel showNotification={props.showNotification} /><DocumentEditorStudio {...props} /></div>;
+export const DocumentEditor: React.FC<React.ComponentProps<typeof DocumentEditorLexical>> = (props) => (
+  <div className="orbidoc-productivity-shell min-w-0">
+    <StudioPowerBar project={props.project} kind="word" showNotification={props.showNotification} />
+    <DocumentEditorLexical {...props} />
+  </div>
+);
