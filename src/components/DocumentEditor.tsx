7b335c3526ad2d3a94e08e9e954a151bc81562ac
KassiusPrime <cassianokaique9@gmail.com>
2026-08-31 22:32:07 -0300

    feat(word): preserve find-replace in Lexical workspace

--- src/components/DocumentEditor.tsx
+++ src/components/DocumentEditor.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { DocumentEditorLexical } from './DocumentEditorLexical';
+import { DocumentFindReplaceBar } from './DocumentFindReplaceBar';
 import { StudioPowerBar } from './StudioPowerBar';
 
 export const DocumentEditor: React.FC<React.ComponentProps<typeof DocumentEditorLexical>> = (props) => (
   <div className="orbidoc-productivity-shell min-w-0">
     <StudioPowerBar project={props.project} kind="word" showNotification={props.showNotification} />
+    <DocumentFindReplaceBar showNotification={props.showNotification} />
     <DocumentEditorLexical {...props} />
   </div>
 );
